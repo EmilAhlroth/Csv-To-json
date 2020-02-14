7b7811d7fb8027eba16faa5ff5ebe2f313f6d7d5
EmilAhlroth <46751805+EmilAhlroth@users.noreply.github.com>
2020-02-14 13:04:09 +0100

    j

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ a,b,c
 const fs = require('fs');
 const csvFilePath ='./exel.csv'
 const csv = require('csvtojson')
+var http = require('http');
 csv()
 .fromFile(csvFilePath)
 .then((jsonObj)=>{
@@ -14,3 +15,12 @@ csv()
      var send = JSON.stringify (jsonObj);
     fs.writeFileSync('./resultat.json', send);
   })
+
+  http.createServer(function (req, res) {
+    //Open a file on the server and return its content:
+    fs.readFile('resultat.json', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+  }).listen(8080);
